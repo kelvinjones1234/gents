d302c854537ec1f67afb3fcc725a66d663cd6764
kelvinjones1234 <praisejournal2@gmail.com>
2026-03-01 18:27:58 +0100

    loader

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,42 +1,3 @@
-// "use client"; 
-
-// import React, { Suspense } from "react"; // 1. Import Suspense
-// import Navbar from "@/app/(general)/components/Navbar";
-// import { ToastProvider } from "@/context/ToastContext";
-// import { CartProvider } from "@/context/CartContext"; 
-// import CartDrawer from "./components/CartDrawer";
-
-// export default function GeneralLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <ToastProvider>
-//       <CartProvider>
-//         <div className="min-h-screen bg-background text-foreground relative">
-//           <Navbar />
-          
-//           {/* 2. Wrap the children in the Suspense boundary */}
-//           <Suspense 
-//             fallback={
-//               <div className="min-h-screen flex items-center justify-center text-[10px] font-bold uppercase tracking-widest text-muted">
-//                 Loading...
-//               </div>
-//             }
-//           >
-//             <main className="">{children}</main>
-//           </Suspense>
-
-//           <CartDrawer />
-//         </div>
-//       </CartProvider>
-//     </ToastProvider>
-//   );
-// }
-
-
-
 "use client"; 
 
 import React, { Suspense } from "react";
@@ -70,7 +31,7 @@ export default function GeneralLayout({
           color="#000" 
           showSpinner={false} 
           shadow="none"
-          height={2}
+          height={4}
         />
         
         <div className="min-h-screen bg-background text-foreground relative">
