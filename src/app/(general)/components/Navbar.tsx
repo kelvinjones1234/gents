d0ae03e1af0efb673ccee4bb8cc70990ee506a68
kelvinjones1234 <praisejournal2@gmail.com>
2026-03-11 17:43:50 +0100

    fixed some logo

--- src/app/(general)/components/Navbar.tsx
+++ src/app/(general)/components/Navbar.tsx
@@ -239,7 +239,7 @@ export default function Navbar() {
                 alt="Gents Collection Store Logo"
                 width={100}
                 height={80}
-                className="object-contain"
+                className="object-contain py-2"
               />
             </Link>
           </div>
